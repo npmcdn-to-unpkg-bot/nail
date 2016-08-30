f6f9ff2a14954c45ceb52d311124bdf2674aa721
jihye <im@jihye.me>
2016-08-30 22:05:47 +0900

    add react.js

--- scripts/nail.js
+++ scripts/nail.js
@@ -4,11 +4,16 @@ console.log('nail.js import ok');
 
 
 var access_token = '2284247885.bb4f32a.47851f2fda564915bbae378c5d9710bb';
+var hashtag = 'nail';
+var count = 4;
 $.ajax({   
-     type: "GET",
-     url: "https://api.instagram.com/v1/tags/nail/media/recent?access_token=" + access_token, 
-     dataType: "jsonp",
+     type: 'GET',
+     dataType: 'jsonp',
+     // url: 'https://api.instagram.com/v1/tags/search?q=k3&access_token=' + access_token, 
+     url: 'https://api.instagram.com/v1/tags/'+hashtag+'/media/recent',
+     data: {access_token: access_token, count: count},
      success: function(result){
+        console.log(result);
 		var html = '';
 		$.each(result.data, function(i, value){
 			html += '<li><img src="'+ value.images.thumbnail.url + '"></li>';
@@ -20,3 +25,6 @@ $.ajax({
        }
      }
 );
+
+
+ 
