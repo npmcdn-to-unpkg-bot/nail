3f0293a621caaf35521f58e43954e81f9165dfbd
jihye <im@jihye.me>
2016-08-31 16:01:35 +0900

    update react code

--- scripts/nail.js
+++ scripts/nail.js
@@ -1,30 +1,62 @@
-console.log('nail.js import ok');
+var Content = React.createClass({
+    loadCommentsFromServer: function(){
+        var access_token = '2284247885.bb4f32a.47851f2fda564915bbae378c5d9710bb';
+        var count = 4;
+
+        $.ajax({   
+            type: 'GET',
+            dataType: 'jsonp',
+            url: this.props.url,
+            data: {access_token: access_token, count: count},
+            success: function(data){
+                this.setState({data: data.data});
+            }.bind(this),
+            error:function(xhr, status, err){
+                console.error('message:'+xhr.responseText);
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function(){
+        return {data: []}
+    },
+    componentDidMount: function(){
+        this.loadCommentsFromServer();
+    },
+    render: function(){
+        return (
+            <div className="content">
+                <h2 className="title"> 최신네일 ㅋㅋ </h2>
+                <ImageList data={this.state.data} />
+            </div>
+        );
+    }
+});
+
+var ImageList = React.createClass({
+    render: function(){
+        var ImageNode = this.props.data.map(function(result){
+            return <Image key={result.id} src={result.images.thumbnail.url} />
+        });
+        return (
+            <ul className="images">
+                {ImageNode}
+            </ul>
+        );
+    }
+});
+
+var Image = React.createClass({
+    render: function(){
+        return (
+            <li className="image"><img src={this.props.src} /></li>
+        );
+    }
+});
 
 
-
-
-var access_token = '2284247885.bb4f32a.47851f2fda564915bbae378c5d9710bb';
-var hashtag = 'nail';
-var count = 4;
-$.ajax({   
-     type: 'GET',
-     dataType: 'jsonp',
-     // url: 'https://api.instagram.com/v1/tags/search?q=k3&access_token=' + access_token, 
-     url: 'https://api.instagram.com/v1/tags/'+hashtag+'/media/recent',
-     data: {access_token: access_token, count: count},
-     success: function(result){
-        console.log(result);
-		var html = '';
-		$.each(result.data, function(i, value){
-			html += '<li><img src="'+ value.images.thumbnail.url + '"></li>';
-		});
-    	$('ul#images').append(html);
-     },
-     error:function(jqXHR, textStatus, error){
-        console.error("message:"+jqXHR.responseText);
-       }
-     }
-);
-
-
- 
+var url= 'https://api.instagram.com/v1/tags/nail/media/recent';
+ReactDOM.render(
+    <Content url={url} />,
+    document.getElementById('content')
+)
--- scripts/react.js
+++ scripts/react.js
@@ -1,14 +1,62 @@
-var CommentBox = React.createClass({
+var Content = React.createClass({
+	getInitialState: function(){
+		return {data: []}
+	},
+	componentDidMount: function(){
+	    var access_token = '2284247885.bb4f32a.47851f2fda564915bbae378c5d9710bb';
+	    var count = 4;
+    	var url= 'https://api.instagram.com/v1/tags/nail/media/recent',
+
+	    $.ajax({   
+	        type: 'GET',
+	        dataType: 'jsonp',
+	        url: url,
+	        data: {access_token: access_token, count: count},
+	        success: function(result){
+	    		var html = '';
+	    		$.each(result.data, function(i, value){
+	    			html += '<li><img src="'+ value.images.thumbnail.url + '"></li>';
+	    		});
+	        	$('ul#images').append(html);
+	        },
+	        error:function(jqXHR, textStatus, error){
+	            console.error("message:"+jqXHR.responseText);
+	        }
+	     }
+	    );
+	}
 	render: function(){
 		return (
-			<div className="commentBox">
-				Hello, world!
+			<div className="content">
+				<h2 className="title"> 최신네일 ㅋㅋ </h2>
+				<ImageList data={this.state.data} />
 			</div>
 		);
 	}
 });
 
+var ImageList = React.createClass({
+	render: function(){
+		var ImageNode = this.props.data.map(function(image){
+			return <Image key={image.name} name={image.name} />
+		});
+		return (
+			<ul className="images">
+				{ImageNode}
+			</ul>
+		);
+	}
+});
+
+var Image = React.createClass({
+	render: function(){
+		return (
+			<li className="image">{this.props.name}</li>
+		);
+	}
+});
+
 ReactDOM.render(
-	<CommentBox />,
+	<Content url={url} />,
 	document.getElementById('content')
-);
+)
