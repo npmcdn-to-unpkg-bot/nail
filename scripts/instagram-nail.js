938e52fa7f05dbd555e3ddbf7dcbc1e944e9a690
jihye <im@jihye.me>
2016-09-01 15:51:40 +0900

    add form

--- scripts/instagram-nail.js
+++ scripts/instagram-nail.js
@@ -17,7 +17,7 @@ var Content = React.createClass({
                 this.setState({data: data.data});
             }.bind(this),
             error:function(xhr, status, err){
-                console.error('message:'+xhr.responseText);
+                console.error('message:' + xhr.responseText);
                 console.error(url, status, err.toString());
             }.bind(this)
         });
@@ -27,14 +27,31 @@ var Content = React.createClass({
     },
     componentDidMount: function(){
         this.loadData();
-        setInterval(this.loadData, this.props.pollInterval)
+        // setInterval(this.loadData, this.props.pollInterval)
+    },
+    handlerCommentSubmit: function(comment){
+        console.log(comment);
     },
     render: function(){
         return (
             <div className="content">
-                <h2>랜덤사진!</h2>
+                <Title> 랜덤 *사진!* </Title>
                 <ImageList data={this.state.data}/>
+                <CommentForm onCommentSubmit={this.handlerCommentSubmit}/>
             </div>
+        );
+    }
+});
+
+var Title = React.createClass({
+    rawMarkup: function(){
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString())
+        return ({__html: rawMarkup});
+    },
+    render: function(){
+        return (
+            <h2 dangerouslySetInnerHTML={this.rawMarkup()} />
         );
     }
 });
@@ -48,6 +65,33 @@ var ImageList = React.createClass({
             <ul className="images">
                 {ImageNode}
             </ul>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    getInitialState: function(){
+        return ({text: ''});
+    },
+    handlerTextChange: function(e){
+        this.setState({text: e.target.value});
+    },
+    handlerSubmit: function(e){
+        e.preventDefault();
+        var text = this.state.text.trim();
+        if(!text){
+            return;
+        }
+        this.props.onCommentSubmit({text: text});
+        this.setState({text: ''});
+    },
+    render: function(){
+        return (
+            <form className="commentForm" onSubmit={this.handlerSubmit}>
+                <input type="text" placeholder="text..." 
+                    onChange={this.handlerTextChange} />
+                <input type="submit" value="Send" />
+            </form>
         );
     }
 });
